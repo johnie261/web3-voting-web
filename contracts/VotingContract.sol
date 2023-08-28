58d405f1a834eb52f0fc5cf77cd74dccd8f205bf
johnie261 <njorogejohn261@gmail.com>
2023-08-28 11:11:54 +0300

    Create a function to create a new candidates and get all candidates created

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -1,6 +1,112 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.9;
 
-contract VotingContract {
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
+// contract VotingContract {
+
+// };
+
+contract Create {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public _voterId;
+    Counters.Counter public _candidateId;
+
+    address public votingOrganizer;
+
+    struct Candidate {
+        uint256 candidateId;
+        string age;
+        string name;
+        string image;
+        uint256 voteCount;
+        address _address;
+        string ipfs;
+    }
+
+    struct Voters {
+        uint256 voter_voterId;
+        string voter_name;
+        string voter_image;
+        address voter_address;
+        uint256 voter_allowed;
+        bool voter_voted;
+        uint256 voter_vote;
+        string voter_ipfs;
+    }
+
+    event candidateCreate (
+        uint256 indexed candidateId,
+        string age,
+        string name,
+        string image,
+        uint256 voteCount,
+        address _address,
+        string ipfs
+    );
+
+    event voterCreated (
+        uint256 indexed voter_voterId,
+        string voter_name,
+        string voter_image,
+        address voter_address,
+        uint256 voter_allowed,
+        bool voter_voted,
+        uint256 voter_vote,
+        string voter_ipfs
+    );
+
+    address[] public candidateAddress;
+    address[] public votersAddress;
+    address[] public votedVoters;
+
+    mapping(address => Candidate) public candidates; 
+    mapping(address => Voters) public voters;
+
+    constructor (){
+        votingOrganizer = msg.sender;
+    }
+
+    function setCandidate(
+        address _address,
+        string memory _age,
+        string memory _name,
+        string memory _image,
+        string memory _ipfs
+    ) public {
+        require(votingOrganizer == msg.sender, "Only organizer can create candidate");
+
+        _candidateId.increment();
+
+        uint256 idNumber = _candidateId.current();
+
+        Candidate storage candidate = candidates[_address];
+
+        candidate.age = _age;
+        candidate.name = _name;
+        candidate.candidateId = idNumber;
+        candidate.image = _image;
+        candidate.voteCount = 0;
+        candidate._address = _address;
+        candidate.ipfs = _ipfs;
+
+        candidateAddress.push(_address);
+
+        emit candidateCreate(
+            idNumber,
+            _age, 
+            _name, 
+            _image, 
+            candidate.voteCount, 
+            _address, 
+            _ipfs
+        );
+    }
+
+    function getCandidate() public view returns (address[] memory) {
+        return candidateAddress;
+    }
 }
